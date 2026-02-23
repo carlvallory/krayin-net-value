ed792a24681e2c3e9a436b719107470726f2f927
Carlos Vallory <carlvallory@gmail.com>
2026-02-23 09:48:54 -0300

    fix 1

--- src/Providers/KrayinNetValueServiceProvider.php
+++ src/Providers/KrayinNetValueServiceProvider.php
@@ -16,44 +16,14 @@ class KrayinNetValueServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
 
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
-
-        $this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'krayinnetvalue');
-
-        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'krayinnetvalue');
-
-        Event::listen('admin.layout.head.after', function($viewRenderEventManager) {
-            $viewRenderEventManager->addTemplate('krayinnetvalue::components.layouts.style');
-        });
+        // No frontend/translation files needed for this backend backend package.
         
         // Listen to Lead creations and updates to sync net_value
         Event::listen('lead.create.after', 'CarlVallory\KrayinNetValue\Listeners\LeadSaveListener@handle');
         Event::listen('lead.update.after', 'CarlVallory\KrayinNetValue\Listeners\LeadSaveListener@handle');
     }
 
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerConfig();
-    }
+    // Registration not required
 
-    /**
-     * Register package config.
-     *
-     * @return void
-     */
-    protected function registerConfig()
-    {
-        $this->mergeConfigFrom(
-            dirname(__DIR__) . '/Config/menu.php', 'menu.admin'
-        );
-
-        $this->mergeConfigFrom(
-            dirname(__DIR__) . '/Config/acl.php', 'acl'
-        );
-    }
+        // No config/menu registration needed.
 }
